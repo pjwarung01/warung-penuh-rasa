82356e7dfe1ff6cee432523264b04e6eccbf701b
Eggi Abnureza <eggi.abnureza@atrbpn.go.id>
2025-04-07 11:30:14 +0700

    menambah fitur search dan shopping cart

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Toggle class active
+// Toggle class active Hamburger Menu
 const navbarNav = document.querySelector(".navbar-nav");
 
 // Ketika Hamburger Menu di Klik
@@ -6,11 +6,45 @@ document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
+// Toggle class active Search-From
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// Toggle class active Shopping Cart
+const shoppingCart = document.querySelector(".shopping-cart");
+
+document.querySelector("#shopping-cart-button").onclick = (e) => {
+  shoppingCart.classList.toggle("active");
+  e.preventDefault();
+};
+
 // Klik diluar sidebar untuk menghilangkan navbar
 const hamburger = document.querySelector("#hamburger-menu");
+const searchButton = document.querySelector("#search-button");
+const sc = document.querySelector("#shopping-cart-button");
 
 document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
   }
 });
+
+document.addEventListener("click", function (e) {
+  if (!searchButton.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
+});
+
+document.addEventListener("click", function (e) {
+  if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove("active");
+  }
+});
+
+//
