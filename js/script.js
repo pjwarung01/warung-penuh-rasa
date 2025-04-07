a43f89a3424cbb42dd94f38051c0c53bca999e42
Eggi Abnureza <eggi.abnureza@atrbpn.go.id>
2025-04-07 21:24:30 +0700

    menambah fitur shooping cart

--- js/script.js
+++ js/script.js
@@ -47,4 +47,26 @@ document.addEventListener("click", function (e) {
   }
 });
 
-//
+// Modal box
+const itemDetailModal = document.querySelector("#item-detail-modal");
+const itemDetailButtons = document.querySelectorAll(".item-detail-button");
+
+itemDetailButtons.forEach((btn) => {
+  btn.onclick = (e) => {
+    itemDetailModal.style.display = "flex";
+    e.preventDefault();
+  };
+});
+
+// klik tombol box
+document.querySelector(".modal .close-icon").onclick = (e) => {
+  itemDetailModal.style.display = "none";
+  e.preventDefault();
+};
+
+// Klik di luar Modal
+window.onclick = (e) => {
+  if (e.target === itemDetailModal) {
+    itemDetailModal.style.display = "none";
+  }
+};
