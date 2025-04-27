eda7191684e04a8a4dc54aa69532454431ec54d9
Eggi Abnureza <eggi.abnureza@atrbpn.go.id>
2025-04-27 09:59:00 +0700

    testing add fitur tombol

--- src/app.js
+++ src/app.js
@@ -67,6 +67,25 @@ document.addEventListener("alpine:init", () => {
   });
 });
 
+// Form Validation
+const checkoutButton = document.querySelector(".checkout-button");
+checkoutButton.disabled = true;
+
+const form = document.querySelector("#checkoutForm");
+
+form.addEventListener("keyup", function () {
+  for(let i = 0; i < form.elements.length; i++;) {
+    if(form.elements[i].value.length !== 0) {
+      checkoutButton.classList.remove("disabled");
+      checkoutButton.classList.add("disabled");
+    } else {
+      return false;
+    }
+  }
+  checkoutButton.disabled = false;
+  checkoutButton.classList.remove("disabled");
+});
+
 // Konversi ke rupiah
 const rupiah = (number) => {
   return new Intl.NumberFormat("id-ID", {
